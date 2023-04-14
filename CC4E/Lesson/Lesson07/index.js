f890d0fc1bf9cfd5c7c7e7a8542d284763d528cb
thangdodinh07096 <thangdodinh07096>
2023-04-14 21:01:49 +0700

    add lesson 07

---
+++ CC4E/Lesson/Lesson07/index.js
@@ -0,0 +1,15 @@
+console.log("Hello Javascript");
+var firstName = "Thang";
+console.log(firstName);
+let x;
+let y;
+let z = 2;
+let t = 2;
+x = 5;
+y = x++;
+z += x;
+t **= x;
+console.log(x);
+console.log(y);
+console.log(z);
+console.log(t);
